16e6da1c8a034fe016a036d804f73e9ed59ba444
Anaesthesia <ligeia1305@yandex.ru>
2019-04-11 00:51:13 +0300

    step4 try3

--- src/bin/even.js
+++ src/bin/even.js
@@ -2,7 +2,7 @@ export const IntRandom = (s, f) => {
   return Math.random() * (f - s);
 };
 export const isEven = {
-if (IntRandom % 2 === 0) {return yes;}
+if ((IntRandom % 2) === 0) {return yes;}
 else {return no;}
 };
 export const evenOrNot = () => {
