7d47f4d567f401bc79875ee95b8fc42fbf6195e1
Anaesthesia <ligeia1305@yandex.ru>
2019-04-11 20:34:12 +0300

    try to fix code1

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import readlineSync from 'readline-sync';
-export default const knowName = () => {
+export const knowName = () => {
 	readlineSynq.question ('May I have your name? ');
 console.log ('Hello, ${knowName}!');
 };
