7c2e35ef79232b536c0fa547a6ae80a7c7e052f3
Anaesthesia <ligeia1305@yandex.ru>
2019-04-12 19:50:06 +0300

    travis one more fix

--- src/bin/calc.js
+++ src/bin/calc.js
@@ -4,12 +4,8 @@ console.log ('Welcome to the Brain Games!');
 knowName ();
 
 export const SumOfRandom = (f,s) => {
-const x = {
-  return Math.random() * (f - s);
-};
-const y = {
-  return Math.random() * (f - s);
-};
+const x = Math.random() * (f - s);
+const y = Math.random() * (f - s);
 const sign = [+, -, *];
-const s = Math.floor(Math.random()*sign.length);
+const z = Math.floor(Math.random()*sign.length);
 }
