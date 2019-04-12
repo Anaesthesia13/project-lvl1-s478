7d042d0dec7664aab41a01ce658774172f4ededa
Anaesthesia <ligeia1305@yandex.ru>
2019-04-12 19:55:53 +0300

    travis fixing calc

--- src/bin/calc.js
+++ src/bin/calc.js
@@ -2,10 +2,3 @@
 import { knowName } from '..';
 console.log ('Welcome to the Brain Games!');
 knowName ();
-
-export const SumOfRandom = (f,s) => {
-const x = Math.random() * (f - s);
-const y = Math.random() * (f - s);
-const sign = [+; -; *];
-const z = Math.floor(Math.random()*sign.length);
-}
