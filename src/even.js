6cc3d118ce7e85aafa14f3b10d2381cb8a21aef0
Anaesthesia <ligeia1305@yandex.ru>
2019-04-13 12:59:10 +0300

    fixing spaces

--- src/even.js
+++ src/even.js
@@ -1,25 +1,25 @@
 import readlineSync from 'readline-sync';
 
 const IntRandom = (s, f) => Math.floor(Math.random() * (f - s));
-export const x = IntRandom (1, 100);
+export const x = IntRandom(1, 100);
 const compare = () => {
-	let i = x % 2;
-if (i === 0) {
-	return true;
-}
-else {
-	return false;
-}
+  let i = x % 2;
+  if (i === 0) {
+    return true;
+  }
+  else {
+    return false;
+  }
 };
 export const isEven = () => {
-if (compare (x) === true) {
-	return 'yes';
-}
-else {
-	return 'no';
-}
+  if (compare (x) === true) {
+    return 'yes';
+  }
+  else {
+    return 'no';
+  }
 };
 export const evenOrNot = () => {
-	readLineSynq.question('Your answer: ');
-	console.log('Answer "yes" if number even otherwise answer "no".');
+  readLineSynq.question('Your answer: ');
+  console.log('Answer "yes" if number even otherwise answer "no".');
 };
